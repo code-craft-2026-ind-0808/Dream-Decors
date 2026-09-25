6ea882a3d3b94b0a3ab7b8a7754460a567afc63c
code-craft-2026-ind-0808 <jessicamarshal.0808@gmail.com>
2026-09-25 19:27:34 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBja7txTxFGPt4_2rA1vDKeqPenq5Wgzgc",
+  authDomain: "event-decors.firebaseapp.com",
+  projectId: "event-decors",
+  storageBucket: "event-decors.firebasestorage.app",
+  messagingSenderId: "249793760545",
+  appId: "1:249793760545:web:1cfcc38909d844e1c20260",
+  measurementId: "G-QZZS80PMHC"
+};
