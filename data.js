05fd153d705f5852ee2cff7f814a8ad1b8d0cf8a
code-craft-2026-ind-0808 <jessicamarshal.0808@gmail.com>
2026-09-25 19:13:21 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,30 @@
+// Dream Decors by Yeswanth - Edit this file daily
+// To add new event: copy one block and paste below
+// Upload photo to GitHub first: Add file > Upload files > upload to /images folder
+
+const events = [
+  {
+    title: "Royal Wedding Setup - Palace Grounds",
+    category: "Wedding",
+    date: "24-Sep-2025",
+    image: "https://images.unsplash.com/photo-1519225421980-715cb0215aed?w=500"
+  },
+  {
+    title: "Birthday Balloon Decor - Whitefield",
+    category: "Birthday",
+    date: "23-Sep-2025",
+    image: "https://images.unsplash.com/photo-1530103862676-de8c9debad1d?w=500"
+  },
+  {
+    title: "Corporate Stage - MG Road",
+    category: "Corporate",
+    date: "22-Sep-2025",
+    image: "https://images.unsplash.com/photo-1505236858219-40719f88ff9d?w=500"
+  },
+  {
+    title: "DJ Night - Koramangala",
+    category: "DJ",
+    date: "21-Sep-2025",
+    image: "https://images.unsplash.com/photo-1492684223066-81342ee5ff30?w=500"
+  }
+];
